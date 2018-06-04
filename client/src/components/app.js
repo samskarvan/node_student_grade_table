b22313a444723cc6073d4bcdf4c3b853114b7f98
samskarvan <samskarvan@gmail.com>
2018-06-04 14:49:42 -0700

    file strucure for client sde of SGT completed

---
+++ client/src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import 'materialize-css/dist/css/materialize.min.css';
+import logo from '../assets/images/logo.svg';
+
+const App = () => (
+    <div>
+        <div className="app">
+            <h1>Welcome to React</h1>
+        </div>
+    </div>
+);
+
+export default App;
