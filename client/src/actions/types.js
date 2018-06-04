87230e2bab431f2936f133d291151aa1dfb1865a
samskarvan <35939429+samskarvan@users.noreply.github.com>
2018-06-04 14:54:58 -0700

    Revert "file structure for client sde of SGT completed"

--- client/src/actions/types.js
+++
@@ -1,8 +0,0 @@
-export default {
-    GET_LIST_DATA: 'get_list_data',
-    ADD_ITEM: 'add_item',
-    GET_SINGLE_ITEM: 'get_single_item',
-    TOGGLE_COMPLETE: 'toggle_complete',
-    DELETE_ITEM: 'delete_item',
-    CLEAR_SINGLE_ITEM: 'clear_single_item'
-};
--- client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import listReducer from './list_reducer';
-import {reducer as formReducer} from 'redux-form';
-
-
-export default combineReducers({
-    list: listReducer,
-    form: formReducer
-});
