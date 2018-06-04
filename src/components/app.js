644d96f878ee7bbd62300a1f748d9855517e43f5
samskarvan <samskarvan@gmail.com>
2018-06-04 14:12:23 -0700

    altered initial css

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,6 @@ import logo from '../assets/images/logo.svg';
 const App = () => (
     <div>
         <div className="app">
-            <img src={logo} className="logo rotate"/>
             <h1>Welcome to React</h1>
         </div>
     </div>
