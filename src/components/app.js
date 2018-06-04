7590f9d993bb6c3c5265d5db00a125ad0e921196
samskarvan <samskarvan@gmail.com>
2018-06-04 14:15:58 -0700

    leaning up package.json

--- src/components/app.js
+++ src/components/app.js
@@ -2,9 +2,6 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import 'materialize-css/dist/css/materialize.min.css';
 import logo from '../assets/images/logo.svg';
-import List from './list'
-import AddItem from './add_item';
-import SingleItem from './single_item';
 
 const App = () => (
     <div>
